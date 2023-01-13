815d6a41e4bebddf98548cdf01425fd258437463
Hussein Jafferjee <hussein@jafferjee.ca>
2023-01-12 18:19:29 -0800

    expose longhorn metrics to prometheus so we can dashboard them

--- terraform/modules/monitoring/main.tf
+++ terraform/modules/monitoring/main.tf
@@ -7,3 +7,37 @@ resource "helm_release" "prometheus-stack" {
   namespace        = "monitoring"
   create_namespace = true
 }
+
+# expose longhorn metrics
+# this took a long time to figure out because the service monitor was not being picked up
+# turns out, we need to have the `release` label, based on:
+# kubectl get prometheuses.monitoring.coreos.com -n monitoring -o jsonpath="{.items[*].spec.serviceMonitorSelector}"
+resource "kubernetes_manifest" "longhorn-service-monitor" {
+  manifest = {
+    apiVersion = "monitoring.coreos.com/v1"
+    kind       = "ServiceMonitor"
+    metadata = {
+      name      = "longhorn-prometheus-servicemonitor"
+      namespace = "monitoring"
+      labels = {
+        name    = "longhorn-prometheus-servicemonitor"
+        release = "prometheus-community"
+      }
+    }
+    spec = {
+      selector = {
+        matchLabels = {
+          app = "longhorn-manager"
+        }
+      }
+      namespaceSelector = {
+        matchNames = ["longhorn-system"]
+      }
+      endpoints = [
+        {
+          port = "manager"
+        }
+      ]
+    }
+  }
+}
